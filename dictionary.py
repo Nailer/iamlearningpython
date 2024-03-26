a3237c175e5acdc4d7e2872401a16ef77523eda2
Success Aje <73680210+successaje@users.noreply.github.com>
2024-03-26 19:19:18 +0100

    further development

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+accouts = {"okx": "btc", "binance": "binance", "trustwallet": [[1, 2, 4], [7, 8, 9]]}
+
+accouts["meta"] = "usdt" # this can help to add an object to the array just by calling the object name and assign a value to it. 
+
+accouts["binance"] = "pepe"
+
+print(accouts)
+
+
+
+
--- lists.py
+++ lists.py
@@ -4,14 +4,18 @@
 # print(lists)
 
 # you can use the '.append' to add an item to the end of a list.
-# .extend help and .pop to remove an item from the list
+# .extend helps to add items to a list and .pop to remove an item from the list and you call the item removed
+# .reverse is used to make a reverse of the items on the list 
+# .sort aranges the items in ascending order
 
-name = ["sam", "bless", "know", "addun"]
-v = name.pop(1)
-print(name)
-print(v)
+# name = ["sam", "bless", "know", [4, 5, 6]]
 
+# print(name[3][1])
+matrix = [["bee", "love", "user"], [1, 2, 4], [5, 6, 7]]
 
+call = [row[0] for row in matrix] # this is known as list-comprehnsion
+
+print(call)
 
 
 
