d100585707169652e15b5435b76b413be02ebb51
Big Mind <53949905+Nailer@users.noreply.github.com>
2024-03-30 03:46:00 +0100

    changes

--- tuple.py
+++ tuple.py
@@ -2,7 +2,7 @@
 
 # print(example[2]) # turples does not support item assignment 
 
-x = set()
+x = set() # sets are just like dictionary that they don't take order when rendering result and it does not repeat the same value even if it's entered seperate times
 
 x.add(4)
 x.add(40)
