a6c7f3355bdd22946748672d1e00f8f5837ae212
Big Mind <53949905+Nailer@users.noreply.github.com>
2024-03-30 03:40:28 +0100

    changes

--- tuple.py
+++ tuple.py
@@ -1,3 +1,10 @@
-example = (4, 5, 6, 7)
+# example = (4, 5, 6, 7) # turples are immutable that means that it can't be changed
 
-print(example)
+# print(example[2]) # turples does not support item assignment 
+
+x = set()
+
+x.add(4)
+x.add(40)
+
+print(x)
